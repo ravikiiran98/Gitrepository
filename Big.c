36ffb92fac8a1cb22a6d04520f07b40bb1bcc3e9
nandeeshnn77-lab <nandeeshnn77@gmail.com>
2026-06-11 10:38:09 +0530

    Add program to compare two integers

---
+++ Big.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    if (num1 > num2) {
+        printf("%d is the bigger number.\n", num1);
+    } else if (num2 > num1) {
+        printf("%d is the bigger number.\n", num2);
+    } else {
+        printf("Both numbers are equal.\n");
+    }
+    
+    return 0;
+}
